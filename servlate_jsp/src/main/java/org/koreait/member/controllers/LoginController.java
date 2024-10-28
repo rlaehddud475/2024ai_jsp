abbb552ef5cf9fd6bb1d8c783661d7476beea301
TwoNewbie <hgwon4071@gmail.com>
2024-10-28 13:13:21 +0900

    20241028

--- servlate_jsp/src/main/java/org/koreait/member/controllers/LoginController.java
+++ servlate_jsp/src/main/java/org/koreait/member/controllers/LoginController.java
@@ -13,13 +13,16 @@ public class LoginController extends HttpServlet {
 
     @Override
     public void destroy() {
-        System.out.println("서블릿 객체가 소멸되기 직전에 1번만 실행");
+        System.out.println("서블릿 객체가 소멸되기 직전에 1번만 실행"); //공통 자원해제, 로그 기록
     }
 
     @Override
-    public void init() throws ServletException {
-        System.out.println("서블릿 객체가 생성되면 최초 1번 실행");
+    public void init() throws ServletException{
+        //ServletConfig의 주요 메서드 : 설정값 조회 메서드
     }
+/*    public void init() throws ServletException {
+        System.out.println("서블릿 객체가 생성되면 최초 1번 실행");//설정 값을 조회할때 최초 1번만 조회후 서블릿 객체 내에서 유지..., 최초 유입 로그...
+    }*/
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,7 +30,7 @@ public class LoginController extends HttpServlet {
     /*    resp.setContentType("text/html");
         resp.setCharacterEncoding("UTF-8");*/
         resp.setContentType("text/html;charset=UTF-8");
-        System.out.println("로그인 페이지!!");
+//        System.out.println("로그인 페이지!!");
         PrintWriter out = resp.getWriter();
         out.write("<h1>로그인</h1>");
     }
