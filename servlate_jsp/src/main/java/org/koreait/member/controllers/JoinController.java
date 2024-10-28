da8819e53365524184059e896894b044cf73a9d0
TwoNewbie <hgwon4071@gmail.com>
2024-10-28 15:04:17 +0900

    20241028

--- servlate_jsp/src/main/java/org/koreait/member/controllers/JoinController.java
+++ servlate_jsp/src/main/java/org/koreait/member/controllers/JoinController.java
@@ -10,6 +10,9 @@ import java.io.IOException;
 public class JoinController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+            String key1 = req.getParameter("key1");
+        String key2 = req.getParameter("key2");
+        System.out.printf("key1=%s%n",key1);
+        System.out.printf("key2=%s%n",key2);
     }
 }
