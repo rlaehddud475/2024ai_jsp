5ff8e43bc0a3262574d7870bac4af80510bdce90
TwoNewbie <hgwon4071@gmail.com>
2024-10-28 13:06:53 +0900

    20241028

--- servlate_jsp/src/main/java/org/koreait/member/controllers/LoginController.java
+++ servlate_jsp/src/main/java/org/koreait/member/controllers/LoginController.java
@@ -7,10 +7,28 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class LoginController extends HttpServlet {
+
+    @Override
+    public void destroy() {
+        System.out.println("서블릿 객체가 소멸되기 직전에 1번만 실행");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("서블릿 객체가 생성되면 최초 1번 실행");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doGet()");
+    /*    resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");*/
+        resp.setContentType("text/html;charset=UTF-8");
         System.out.println("로그인 페이지!!");
+        PrintWriter out = resp.getWriter();
+        out.write("<h1>로그인</h1>");
     }
 }
