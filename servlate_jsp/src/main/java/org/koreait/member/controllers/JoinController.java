c2fada28b4f5ba3c934c589379e5b5a2284dd91e
TwoNewbie <hgwon4071@gmail.com>
2024-10-28 14:56:49 +0900

    20241028

--- servlate_jsp/src/main/java/org/koreait/member/controllers/JoinController.java
+++ servlate_jsp/src/main/java/org/koreait/member/controllers/JoinController.java
@@ -1,6 +1,15 @@
 package org.koreait.member.controllers;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 public class JoinController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
 }
